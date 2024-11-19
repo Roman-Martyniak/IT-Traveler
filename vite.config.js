d8f86f8e69897a514a666a2d1f84ffbfac3ea7f4
Roman-Martyniak <roman.martunyak@gmail.com>
2024-11-19 17:31:28 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/IT-traveler/',
+  plugins: [vue(), ghPages()],
+  base: '/IT-Traveler/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
